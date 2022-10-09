dc2672557dd006fbe35f4f48dff7a6b26a66ef49
Gill Erick <ogayogill95@gmail.com>
2022-10-09 12:14:55 +0300

    FAN: Create helper method to calculate median

--- 8. fraudulent_activity_notifications.py
+++ 8. fraudulent_activity_notifications.py
@@ -2,6 +2,15 @@ def activity_notifications(expenditure: list[int], d: int):
     pass
 
 
+def median_calculator(arr: list[int]):
+    sorted_arr = sorted(arr)
+    length = len(arr)
+    if length % 2 == 0:
+        return sum(sorted_arr[(length // 2 - 1):length // 2 + 1]) / 2
+    else:
+        return sorted_arr[length // 2]
+
+
 if __name__ == "__main__":
     n, d = input().split()
     expenditure = list(map(int, input().rstrip().split()))[:n]
