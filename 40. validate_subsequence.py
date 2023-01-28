37ef4a65fcc51585c9d84857b73d8afeed98bc17
Gill Erick <ogayogill95@gmail.com>
2023-01-28 22:07:34 +0300

    Simplify brute force solution for ValidateSubsequence problem

--- 40. validate_subsequence.py
+++ 40. validate_subsequence.py
@@ -2,18 +2,17 @@ import unittest
 
 
 def validate_subsequence(array, subsequence):
-    j = 0
+    seq_index = 0
     for i in range(len(array)):
-        if array[i] == subsequence[j]:
-            j += 1
-        if j == len(subsequence):
-            return True
-    return False
+        if array[i] == subsequence[seq_index]:
+            seq_index += 1
+    return seq_index == len(subsequence)
 
 
 class TestValidateSubsequence(unittest.TestCase):
     def test_validate_subsequence(self):
         self.assertEqual(True, validate_subsequence([5, 1, 22, 25, 6, -1, 8, 10], [5, 1, 22, 25, 6, -1, 8, 10]))
+        self.assertEqual(True, validate_subsequence([5, 1, 22, 25, 6, -1, 8, 10], [1, 6, 10]))
 
 
 if __name__ == "__main__":
