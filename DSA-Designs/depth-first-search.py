a37bbcb64e3c91a67fa49a531ee37e3ccc4b2a70
Gill Erick <ogayogill95@gmail.com>
2023-01-30 10:36:53 +0300

    Add unit tests for DFS

--- DSA-Designs/depth-first-search.py
+++ DSA-Designs/depth-first-search.py
@@ -2,6 +2,7 @@
 Time complexity - O(V+E) |  Space complexity - O(V)
 We are adding a bunch of frames on the call stack for each of the calls on the BFS for each of the vertices(V)
 """
+import unittest
 
 
 class Node:
@@ -13,8 +14,20 @@ class Node:
         self.children.append(Node(name))
         return self
 
-    def depth_first_search(self, array):
-        array.append(self.name)
-        for child in self.children:
-            child.depth_first_search(array)
-        return array
+
+def depth_first_search(node, array):
+    array.append(node.name)
+    for child in node.children:
+        child.depth_first_search(array)
+    return array
+
+
+class TestDepthFirstSearch(unittest.TestCase):
+    def test_depth_first_search(self):
+        node = Node(1)
+        node.children = [1, 2, 3, [23, 56, 43, 7, [67, 34, 21, 90]], 67, 89]
+        self.assertEqual([], depth_first_search(node, [45]))
+
+
+if __name__ == "__main__":
+    unittest.main()
