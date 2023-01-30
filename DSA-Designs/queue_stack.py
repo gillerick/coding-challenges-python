e295116d048ef9c70f904513d154f622a7d7d702
Gill Erick <ogayogill95@gmail.com>
2023-01-30 11:24:51 +0300

    ADT Queue-Stack: Implement front

--- DSA-Designs/queue_stack.py
+++ DSA-Designs/queue_stack.py
@@ -1,24 +1,45 @@
+class EmptyQueueException(Exception):
+    pass
+
+
 class QueueStack:
     """FIFO queue implementation using list as the underlying data structure."""
 
+    DEFAULT_CAPACITY = 10
+
     def __init__(self):
-        self._data = []
+        """Create an empty queue."""
+        self._data = [None] * QueueStack.DEFAULT_CAPACITY
+        self._size = 0
+        self._front = 0
 
     def __len__(self):
-        return len(self._data)
+        """Return the number of elements in the queue."""
+        return self._size
 
     def enqueue(self, element):
         """Adds element to the back of the queue"""
         pass
 
     def dequeue(self, element):
-        """Removes and returns first element from the queue"""
+        """
+        Remove and return the first element of the queue.
+        Raise EmptyQueueException if queue is empty
+        """
+        if self.is_empty():
+            raise EmptyQueueException("Queue is empty")
         pass
 
     def is_empty(self):
-        """Checks if the queue is empty"""
-        return self.__len__() == 0
+        """Return True if the queue is empty"""
+        return self._size == 0
 
     def first(self):
-        """Returns reference to the element at the front of the queue without removing it"""
-        return self._data[0]
+        """
+        Returns reference to the element at the front of the queue without removing it.
+        Raise EmptyQueueException if queue is empty.
+        """
+
+        if self.is_empty():
+            raise EmptyQueueException("Queue is empty")
+        return self._data[self._front]
