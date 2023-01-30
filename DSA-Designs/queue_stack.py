39163788a751ca68c69de3d00c9775030c2d661b
Gill Erick <ogayogill95@gmail.com>
2023-01-30 14:01:54 +0300

    ADT Queue-Stack: Implement dequeue

--- DSA-Designs/queue_stack.py
+++ DSA-Designs/queue_stack.py
@@ -19,23 +19,24 @@ class QueueStack:
 
     def enqueue(self, element):
         """Adds element to the back of the queue"""
-        # Check the first None element, if any append element at position
-        # If not, increase size of queue and insert element at first None position
         if self._size == len(self._data):
             self._resize(2 * len(self._data))  # double the size of the array
         avail = (self._front + self._size) % len(self._data)
         self._data[avail] = element
         self._size += 1
 
-    def dequeue(self, element):
+    def dequeue(self):
         """
         Remove and return the first element of the queue.
         Raise EmptyQueueException if queue is empty
         """
-        # 1. Check if queue is empty
         if self.is_empty():
             raise EmptyQueueException("Queue is empty")
-        pass
+        answer = self._data[self._front]
+        self._data[self._front] = None  # help garbage collection
+        self._front = (self._front + 1) % len(self._data)
+        self._size -= 1
+        return answer
 
     def is_empty(self):
         """Return True if the queue is empty"""
@@ -46,7 +47,6 @@ class QueueStack:
         Returns reference to the element at the front of the queue without removing it.
         Raise EmptyQueueException if queue is empty.
         """
-
         if self.is_empty():
             raise EmptyQueueException("Queue is empty")
         return self._data[self._front]
