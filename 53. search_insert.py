dda4ac54d7c40ea59374763cf9f548b16eb6ef6a
Gill Erick <ogayogill95@gmail.com>
2023-02-01 12:52:39 +0300

    Add solutin for searchInsert problem

---
+++ 53. search_insert.py
@@ -0,0 +1,27 @@
+import unittest
+
+
+def search_insert(nums: list[int], target: int):
+    left_idx = 0
+    right_idx = len(nums) - 1
+    while left_idx < right_idx:
+        mid = (left_idx + right_idx) // 2
+        if nums[mid] == target:
+            return mid
+        elif nums[mid] > target:
+            right_idx = mid - 1
+        else:
+            left_idx = mid + 1
+
+    return left_idx
+
+
+class TestSearchInsert(unittest.TestCase):
+    def test_search_insert(self):
+        self.assertEqual(1, search_insert([2, 3, 5, 6], 1))
+        self.assertEqual(1, search_insert([1, 3, 5, 6], 2))
+        self.assertEqual(3, search_insert([1, 8, 10, 90], 29))
+
+
+if __name__ == "__main__":
+    unittest.main()
