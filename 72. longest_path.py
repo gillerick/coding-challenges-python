af4b557e24afba3cef471a13ca47899241edaa1c
Gill Erick <ogayogill95@gmail.com>
2023-02-08 18:29:29 +0300

    LongestPath problem: Refactor for readability

--- 72. longest_path.py
+++ 72. longest_path.py
@@ -2,34 +2,24 @@ import unittest
 
 
 def longest_path(str_array: list[str]) -> int:
-    # 1. Construct a matrix from the array passed
-    # 2. Loop through the matrix from the first cell to the last cell
-    # 3. Have a variable that keeps track of the longest path
-
-    # Helper function that searches through matrix using Depth First Search technique
-    def dfs(x, y, previous):
+    def dfs(row, column, previous):
         nonlocal longestPath
-        # Handle base case(s)
-        if x < 0 or x >= len(matrix) or y < 0 or y >= len(matrix[0]) or matrix[x][y] <= previous:
+        if row < 0 or row >= len(matrix) or column < 0 or column >= len(matrix[0]) or matrix[row][column] <= previous:
             return 0
 
-        currentLength = 1 + max(dfs(x - 1, y, matrix[x][y]),
-                                dfs(x + 1, y, matrix[x][y]),
-                                dfs(x, y - 1, matrix[x][y]),
-                                dfs(x, y + 1, matrix[x][y]))
+        currentLength = 1 + max(dfs(row - 1, column, matrix[row][column]),
+                                dfs(row + 1, column, matrix[row][column]),
+                                dfs(row, column - 1, matrix[row][column]),
+                                dfs(row, column + 1, matrix[row][column]))
 
-        # Update the longest path
         longestPath = max(longestPath, currentLength)
         return currentLength
 
-    # We cast this to an int as it a string from the input
     matrix = [[int(x) for x in row] for row in str_array]
     longestPath = 0
-    for i in range(len(matrix)):
-        for j in range(len(matrix[0])):
-            # We initialize the previous longest path to the smallest number since we are starting the iteration
-            dfs(i, j, float('-inf'))
-            # Here, we subtract 1 since the path length is one less the number of cells in the connection
+    for row in range(len(matrix)):
+        for col in range(len(matrix[0])):
+            dfs(row, col, float('-inf'))
     return longestPath - 1
 
 
