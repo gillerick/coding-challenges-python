b0ae2d01a7bcda07d8b1a2588566df8abc6d5dee
Gill Erick <ogayogill95@gmail.com>
2023-02-12 12:45:57 +0300

    Move BestTimeToBuyAndSellStock solutions to a single file

--- 65. best_time_to_buy_and_sell_stock.py
+++ 65. best_time_to_buy_and_sell_stock.py
@@ -14,8 +14,25 @@ def best_time_to_buy_and_sell_stock(prices: list[int]):
     return maximum_profit
 
 
+# O(n) time | O(1) space - optimal
+def best_time_to_buy_and_sell_stock_kadane(prices: list[int]):
+    maximum_profit = 0
+    minimum_buy = prices[0]
+    for price in prices:
+        minimum_buy = min(minimum_buy, price)
+        current_profit = price - minimum_buy
+        maximum_profit = max(current_profit, maximum_profit)
+
+    return maximum_profit
+
+
 class TestBestTimeToBuyAndSellStock(unittest.TestCase):
     def test_best_time_to_buy_and_sell_stock(self):
+        self.assertEqual(0, best_time_to_buy_and_sell_stock([9, 8, 5, 4, 1]))
+        self.assertEqual(5, best_time_to_buy_and_sell_stock([7, 1, 5, 3, 6, 4]))
+        self.assertEqual(0, best_time_to_buy_and_sell_stock([7, 6, 4, 3, 1]))
+
+    def test_best_time_to_buy_and_sell_stock_kadane(self):
         self.assertEqual(0, best_time_to_buy_and_sell_stock([9, 8, 5, 4, 1]))
         self.assertEqual(5, best_time_to_buy_and_sell_stock([7, 1, 5, 3, 6, 4]))
         self.assertEqual(0, best_time_to_buy_and_sell_stock([7, 6, 4, 3, 1]))
