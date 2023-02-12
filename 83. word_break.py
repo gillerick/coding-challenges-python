7b20bf2ac63c6d8a0578ce75c31033883527031f
Gill Erick <ogayogill95@gmail.com>
2023-02-12 18:44:15 +0300

    Add DP solution for WordBreak problem

--- 83. word_break.py
+++ 83. word_break.py
@@ -8,10 +8,30 @@ def word_break(s, word_dict):
     return True
 
 
+def word_break_dp(s, word_dict):
+    n = len(s)
+    dp = [False] * (n + 1)
+    dp[0] = True
+
+    for i in range(1, n + 1):
+        for j in range(i):
+            if dp[j] and s[j:i] in word_dict:
+                dp[i] = True
+                break
+
+    return dp[-1]
+
+
 class TestWordBreak(unittest.TestCase):
     def test_word_break(self):
-        self.assertEqual(True, word_break("leetcode", {"leet", "code"}))
-        self.assertEqual(True, word_break("applepenapple", {"apple", "pen"}))
+        self.assertEqual(True, word_break("leetcode", ["leet", "code"]))
+        self.assertEqual(True, word_break("applepenapple", ["apple", "pen"]))
+        # self.assertEqual(False, word_break("catsandog", ["cats", "dog", "sand", "and", "cat"]))  Test fails
+
+    def test_word_break_dp(self):
+        self.assertEqual(True, word_break_dp("leetcode", ["leet", "code"]))
+        self.assertEqual(True, word_break_dp("applepenapple", ["apple", "pen"]))
+        self.assertEqual(False, word_break_dp("catsandog", ["cats", "dog", "sand", "and", "cat"]))
 
 
 if __name__ == "__main__":
