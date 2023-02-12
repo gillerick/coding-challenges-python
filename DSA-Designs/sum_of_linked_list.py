1b3669ef4be25e1aa6af7a587a3c8f7c39f7a91d
Gill Erick <ogayogill95@gmail.com>
2023-02-12 12:43:10 +0300

    Add alternative solution for SumOfLinkedList problem

--- DSA-Designs/sum_of_linked_list.py
+++ DSA-Designs/sum_of_linked_list.py
@@ -7,9 +7,9 @@ O(max(n,m)) time | O(max(n,m)) time
 
 
 class LinkedList:
-    def __init__(self, value, n=None):
+    def __init__(self, value=None, next_node=None):
         self.value = value
-        self.next = n
+        self.next = next_node
 
 
 def sum_of_linked_list(linked_list_one: LinkedList, linked_list_two: LinkedList):
@@ -37,6 +37,45 @@ def sum_of_linked_list(linked_list_one: LinkedList, linked_list_two: LinkedList)
     return new_linked_list_head_pointer.next
 
 
+def sum_of_linked_list_reversal_technique(linked_list_one, linked_list_two):
+    linked_list_one = reverse_linked_list(linked_list_one)
+    linked_list_two = reverse_linked_list(linked_list_two)
+
+    dummy_head = LinkedList(0)
+    current = dummy_head
+    carry = 0
+
+    while linked_list_one or linked_list_two:
+        x = linked_list_one.val if linked_list_one else 0
+        y = linked_list_two.val if linked_list_two else 0
+        total = carry + x + y
+        carry = total // 10
+        current.next = LinkedList(total % 10)
+        current = current.next
+        if linked_list_one:
+            linked_list_one = linked_list_one.next
+        if linked_list_two:
+            linked_list_two = linked_list_two.next
+
+    if carry > 0:
+        current.next = LinkedList(carry)
+
+    return reverse_linked_list(dummy_head.next)
+
+
+def reverse_linked_list(head_node):
+    previous_node = None
+    current_node = head_node
+
+    while current_node is not None:
+        next_node = current_node.next
+        current_node.next = previous_node
+        previous_node = current_node
+        current_node = next_node
+
+    return previous_node
+
+
 class TestSumOfLinkedList(unittest.TestCase):
     def test_sum_of_linked_list(self):
         linked_list_one = LinkedList(2, LinkedList(4, LinkedList(7, LinkedList(1))))
@@ -44,6 +83,15 @@ class TestSumOfLinkedList(unittest.TestCase):
         results = sum_of_linked_list(linked_list_one, linked_list_two)
         self.assertEqual(1, results.value)
         self.assertEqual(9, results.next.value)
+        self.assertEqual(2, results.next.next.value)
+        self.assertEqual(2, results.next.next.next.value)
+
+    def test_sum_of_linked_list_reversal_technique(self):
+        linked_list_one = LinkedList(2, LinkedList(3, LinkedList(7, LinkedList(1))))
+        linked_list_two = LinkedList(9, LinkedList(3, LinkedList(5)))
+        results = sum_of_linked_list_reversal_technique(linked_list_one, linked_list_two)
+        self.assertEqual(1, results.value)
+        self.assertEqual(7, results.next.value)
         self.assertEqual(2, results.next.next.value)
         self.assertEqual(2, results.next.next.next.value)
 
