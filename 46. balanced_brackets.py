ef7845af1175e6bad034748a72419b1972ee44d7
Gill Erick <ogayogill95@gmail.com>
2023-02-13 21:33:58 +0300

    Move BalancedBracket solutions into a single file

--- 46. balanced_brackets.py
+++ 46. balanced_brackets.py
@@ -14,11 +14,32 @@ def balanced_brackets(string):
     return not stack
 
 
+# O(n) time | O(n) space
+def balanced_brackets_opt(string):
+    bracket_pairs = {"}": "{", "]": "[", ")": "("}
+    stack = {}
+    stack_idx = -1
+    for character in string:
+        if character in bracket_pairs.values():
+            stack_idx += 1
+            stack[stack_idx] = character
+        elif character in bracket_pairs.keys():
+            if not stack or stack[stack_idx] != bracket_pairs[character]:
+                return False
+            stack_idx -= 1
+    return stack_idx == -1
+
+
 class TestBalancedBrackets(unittest.TestCase):
     def test_balanced_brackets(self):
         self.assertEqual(False, balanced_brackets("{[[[[({(}))]]]]}"))
         self.assertEqual(False, balanced_brackets("()[]{}{"))
         self.assertEqual(True, balanced_brackets("([])(){}(())()()"))
+
+    def test_balanced_brackets_optimized(self):
+        self.assertEqual(False, balanced_brackets_opt("{[[[[({(}))]]]]}"))
+        self.assertEqual(False, balanced_brackets_opt("()[]{}{"))
+        self.assertEqual(True, balanced_brackets_opt("([])(){}(())()()"))
 
 
 if __name__ == "__main__":
