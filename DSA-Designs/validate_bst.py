5062d2cbce6153d7cd701474112dfd24888a606c
Gill Erick <ogayogill95@gmail.com>
2023-02-13 19:46:19 +0300

    Move IsValidBst solutions to a single file

--- DSA-Designs/validate_bst.py
+++ DSA-Designs/validate_bst.py
@@ -19,11 +19,26 @@ def is_valid_bst(root: TreeNode, lower=float('-inf'), upper=float('inf')):
     return is_valid_bst(root.left, lower, root.value) and is_valid_bst(root.right, root.value, upper)
 
 
+def is_valid_bst_1(root: TreeNode, min_value=float('-inf'), max_value=float('inf')):
+    if root is None:
+        return True
+    if not root.value > min_value or not root.value < max_value:
+        return False
+    is_left_valid = is_valid_bst(root.left, min_value, root.value)
+    is_right_valid = is_valid_bst(root.right, root.value, max_value)
+    return is_left_valid and is_right_valid
+
+
 class TestIsValidBst(unittest.TestCase):
     def test_is_valid_bst(self):
         self.assertEqual(True, is_valid_bst(TreeNode(15, TreeNode(10, None, None), None)))
         self.assertEqual(True, is_valid_bst(TreeNode(10, None, None)))
         self.assertEqual(False, is_valid_bst(TreeNode(10, TreeNode(17, None, None), TreeNode(15, None, None))))
+
+    def is_valid_bst_1(self):
+        self.assertEqual(True, is_valid_bst_1(TreeNode(15, TreeNode(10, None, None), None)))
+        self.assertEqual(True, is_valid_bst_1(TreeNode(10, None, None)))
+        self.assertEqual(False, is_valid_bst_1(TreeNode(10, TreeNode(17, None, None), TreeNode(15, None, None))))
 
 
 if __name__ == "__main__":
