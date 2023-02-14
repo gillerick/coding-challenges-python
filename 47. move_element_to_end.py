40b494d9fd9afe0dccd14ccc10e3af62ed038911
Gill Erick <ogayogill95@gmail.com>
2023-02-14 13:22:17 +0300

    Solve MoveElementToTheEnd using single pointer technique

--- 47. move_element_to_end.py
+++ 47. move_element_to_end.py
@@ -13,11 +13,17 @@ def move_element_to_the_end(array, to_move):
     return array
 
 
-"""
-    O(n/2) ~ O(n) time | O(1) space
-"""
+# O(n) time | O(1) space
+def move_element_to_the_end_pivot(array, to_move):
+    pivot = 0
+    for i in range(len(array)):
+        if array[i] != to_move:
+            array[i], array[pivot] = array[pivot], array[i]
+            pivot += 1
+    return array
 
 
+# O(n/2) ~ O(n) time | O(1) space NB: Most efficient
 def move_element_to_the_end_two_pointer(array, to_move):
     left_idx = 0
     right_idx = len(array) - 1
@@ -44,12 +50,22 @@ class TestMoveElementToTheEnd(unittest.TestCase):
         self.assertEqual([1, 2, 4, 5, 3], move_element_to_the_end([3, 1, 2, 4, 5], 3))
 
     def test_move_element_to_the_end_two_pointer(self):
-        self.assertEqual([], move_element_to_the_end([], 6))
-        self.assertEqual({3, 3, 3, 3, 3, 3}, set(move_element_to_the_end([3, 3, 3, 3, 3, 3], 3)))
-        self.assertSetEqual({1, 3, 4, 2, 2, 2, 2, 2}, set(move_element_to_the_end([2, 1, 2, 2, 2, 3, 4, 2], 2)))
+        self.assertEqual([], move_element_to_the_end_two_pointer([], 6))
+        self.assertEqual({3, 3, 3, 3, 3, 3}, set(move_element_to_the_end_two_pointer([3, 3, 3, 3, 3, 3], 3)))
+        self.assertSetEqual({1, 3, 4, 2, 2, 2, 2, 2},
+                            set(move_element_to_the_end_two_pointer([2, 1, 2, 2, 2, 3, 4, 2], 2)))
         self.assertEqual({1, 2, 3, 4, 6, 7, 8, 9, 10, 11, 12, 5, 5, 5, 5, 5, 5},
-                         set(move_element_to_the_end([5, 5, 5, 5, 5, 5, 1, 2, 3, 4, 6, 7, 8, 9, 10, 11, 12], 5)))
-        self.assertEqual({1, 2, 4, 5, 3}, set(move_element_to_the_end([3, 1, 2, 4, 5], 3)))
+                         set(move_element_to_the_end_two_pointer([5, 5, 5, 5, 5, 5, 1, 2, 3, 4, 6, 7, 8, 9, 10, 11, 12],
+                                                                 5)))
+        self.assertEqual({1, 2, 4, 5, 3}, set(move_element_to_the_end_two_pointer([3, 1, 2, 4, 5], 3)))
+
+    def test_move_element_to_the_end_pivot(self):
+        self.assertEqual([], move_element_to_the_end_pivot([], 6))
+        self.assertEqual({3, 3, 3, 3, 3, 3}, set(move_element_to_the_end_pivot([3, 3, 3, 3, 3, 3], 3)))
+        self.assertSetEqual({1, 3, 4, 2, 2, 2, 2, 2}, set(move_element_to_the_end_pivot([2, 1, 2, 2, 2, 3, 4, 2], 2)))
+        self.assertEqual({1, 2, 3, 4, 6, 7, 8, 9, 10, 11, 12, 5, 5, 5, 5, 5, 5},
+                         set(move_element_to_the_end_pivot([5, 5, 5, 5, 5, 5, 1, 2, 3, 4, 6, 7, 8, 9, 10, 11, 12], 5)))
+        self.assertEqual({1, 2, 4, 5, 3}, set(move_element_to_the_end_pivot([3, 1, 2, 4, 5], 3)))
 
 
 if __name__ == "__main__":
