8eb487fef999f0db76e7c7d2e03572b0bde52664
Gill Erick <ogayogill95@gmail.com>
2023-03-03 19:19:50 +0300

    Solve SearchInsert problem

--- 53. search_insert.py
+++ 53. search_insert.py
@@ -2,24 +2,21 @@ import unittest
 
 
 def search_insert(nums: list[int], target: int):
-    left_idx = 0
-    right_idx = len(nums) - 1
-    while left_idx < right_idx:
-        mid = (left_idx + right_idx) // 2
-        if nums[mid] == target:
-            return mid
-        elif nums[mid] > target:
-            right_idx = mid - 1
-        else:
-            left_idx = mid + 1
-
-    return left_idx
+    n = len(nums)
+    for i in range(n):
+        current_num = nums[i]
+        if current_num == target:
+            return i
+        elif current_num > target:
+            return i
+    return n
 
 
 class TestSearchInsert(unittest.TestCase):
     def test_search_insert(self):
         self.assertEqual(0, search_insert([2, 3, 5, 6], 1))
         self.assertEqual(1, search_insert([1, 3, 5, 6], 2))
+        self.assertEqual(4, search_insert([1, 3, 5, 6], 7))
         self.assertEqual(3, search_insert([1, 8, 10, 90], 29))
 
 
--- DSA-Designs/depth-first-search.py
+++ DSA-Designs/depth-first-search.py
@@ -1,3 +1,5 @@
+
+
 """All the nodes are also known as vertices
 Time complexity - O(V+E) |  Space complexity - O(V)
 We are adding a bunch of frames on the call stack for each of the calls on the BFS for each of the vertices(V)
