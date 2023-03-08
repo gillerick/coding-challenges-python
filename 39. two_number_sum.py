f96739af56e5b510378b793b54aeba6c71b8ba73
Gill Erick <ogayogill95@gmail.com>
2023-03-08 10:06:42 +0300

    Move TestTwoNumberSum solutions into a single file

--- 39. two_number_sum.py
+++ 39. two_number_sum.py
@@ -11,6 +11,29 @@ def two_number_sum(array, target_sum):
     return []
 
 
+# Space complexity -  O(1)
+# Time complexity -  O(N)
+def two_number_sum_hashmap_memo(array, target_sum):
+    nums = {}
+    for num in array:
+        potential_target = target_sum - num
+        if potential_target in nums:
+            return [potential_target, num]
+        else:
+            nums[num] = True
+    return []
+
+
+# Space complexity -  O(1)
+# Time complexity -  O(N)
+def two_number_sum_array_memo(array, target_sum):
+    for element in array:
+        target = target_sum - element
+        if target in array and target is not element:
+            return [element, target]
+    return []
+
+
 class TestTwoNumberSum(unittest.TestCase):
     def test_two_number_sum(self):
         self.assertEqual([11, -1], two_number_sum([3, 5, -4, 8, 11, 1, -1, 6], 10))
@@ -18,6 +41,20 @@ class TestTwoNumberSum(unittest.TestCase):
         self.assertEqual([4, 1], two_number_sum([4, 6, 1], 5))
         self.assertEqual([6, -3], two_number_sum([4, 6, 1, -3], 3))
         self.assertEqual([8, 9], two_number_sum([1, 2, 3, 4, 5, 6, 7, 8, 9], 17))
+
+    def test_two_number_sum_hashmap_memo(self):
+        self.assertEqual([4, 6], two_number_sum_hashmap_memo([4, 6], 10))
+        self.assertEqual([11, -1], two_number_sum_hashmap_memo([3, 5, -4, 8, 11, 1, -1, 6], 10))
+        self.assertEqual([4, 1], two_number_sum_hashmap_memo([4, 6, 1], 5))
+        self.assertEqual([6, -3], two_number_sum_hashmap_memo([4, 6, 1, -3], 3))
+        self.assertEqual([8, 9], two_number_sum_hashmap_memo([1, 2, 3, 4, 5, 6, 7, 8, 9], 17))
+
+    def test_two_number_sum_array_memo(self):
+        self.assertEqual([4, 6], two_number_sum_array_memo([4, 6], 10))
+        self.assertEqual([4, 1], two_number_sum_array_memo([4, 6, 1], 5))
+        self.assertEqual([11, -1], two_number_sum_array_memo([3, 5, -4, 8, 11, 1, -1, 6], 10))
+        self.assertEqual([6, -3], two_number_sum_array_memo([4, 6, 1, -3], 3))
+        self.assertEqual([8, 9], two_number_sum_array_memo([1, 2, 3, 4, 5, 6, 7, 8, 9], 17))
 
 
 if __name__ == "__main__":
