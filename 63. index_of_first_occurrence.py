e4d7e87286173d5bdb82db2a9ec483dc225cc8a7
Gill Erick <ogayogill95@gmail.com>
2023-05-04 23:32:48 +0300

    Fix wrong test

--- 63. index_of_first_occurrence.py
+++ 63. index_of_first_occurrence.py
@@ -9,7 +9,7 @@ def index_of_first_occurrence(haystack, needle):
     elif haystack == needle:
         return 0
 
-    for i in range(len(haystack)):
+    for i in range(n):
         pivot = 0
         while pivot < n:
             potential_needle = haystack[pivot:pivot + len(needle)]
@@ -22,7 +22,7 @@ def index_of_first_occurrence(haystack, needle):
 
 class TestIndexOfFirstOccurrence(unittest.TestCase):
     def test_index_of_first_occurrence(self):
-        self.assertEqual(2, index_of_first_occurrence("sadbutsad", "sad"))
+        self.assertEqual(0, index_of_first_occurrence("sadbutsad", "sad"))
         self.assertEqual(2, index_of_first_occurrence("hello", "ll"))
         self.assertEqual(-1, index_of_first_occurrence("randomstring", "boy"))
 
