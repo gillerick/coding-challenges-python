fdae7bef73e019ee3a8f1dfd03f0cfac0fd82fa2
gillerick <ogayogill95@gmail.com>
2024-06-30 22:14:10 +0300

    Add unit tests for merge two sorted linked lists problem

--- DSA-Designs/merge_two_sorted_linked_lists.py
+++ DSA-Designs/merge_two_sorted_linked_lists.py
@@ -30,3 +30,80 @@ class MergeTwoLists(object):
             current = current.next
         current.next = list1 or list2
         return head.next
+
+
+def list_to_nodes(lst):
+    """ Helper function to convert list to ListNode chain. """
+    head = ListNode(lst[0])
+    current = head
+    for val in lst[1:]:
+        current.next = ListNode(val)
+        current = current.next
+    return head
+
+
+def nodes_to_list(node):
+    """ Helper function to convert ListNode chain to list. """
+    lst = []
+    while node:
+        lst.append(node.val)
+        node = node.next
+    return lst
+
+
+class TestMergeTwoLists(unittest.TestCase):
+    def test_merge_two_empty_lists(self):
+        l1 = None
+        l2 = None
+        result = MergeTwoLists.merge_two_lists(l1, l2)
+        self.assertIsNone(result)
+
+    def test_merge_one_empty_list(self):
+        l1 = list_to_nodes([1, 3, 5])
+        l2 = None
+        expected = [1, 3, 5]
+        result = MergeTwoLists.merge_two_lists(l1, l2)
+        self.assertEqual(nodes_to_list(result), expected)
+
+        l1 = None
+        l2 = list_to_nodes([2, 4, 6])
+        expected = [2, 4, 6]
+        result = MergeTwoLists.merge_two_lists(l1, l2)
+        self.assertEqual(nodes_to_list(result), expected)
+
+    def test_merge_two_non_empty_lists(self):
+        l1 = list_to_nodes([1, 2, 4])
+        l2 = list_to_nodes([1, 3, 4])
+        expected = [1, 1, 2, 3, 4, 4]
+        result = MergeTwoLists.merge_two_lists(l1, l2)
+        self.assertEqual(nodes_to_list(result), expected)
+
+        l1 = list_to_nodes([5, 6, 7])
+        l2 = list_to_nodes([1, 2, 3])
+        expected = [1, 2, 3, 5, 6, 7]
+        result = MergeTwoLists.merge_two_lists(l1, l2)
+        self.assertEqual(nodes_to_list(result), expected)
+
+    def test_merge_lists_with_duplicates(self):
+        l1 = list_to_nodes([1, 3, 5])
+        l2 = list_to_nodes([1, 3, 5])
+        expected = [1, 1, 3, 3, 5, 5]
+        result = MergeTwoLists.merge_two_lists(l1, l2)
+        self.assertEqual(nodes_to_list(result), expected)
+
+    def test_merge_differently_sized_lists(self):
+        l1 = list_to_nodes([1, 3, 5])
+        l2 = list_to_nodes([2, 4])
+        expected = [1, 2, 3, 4, 5]
+        result = MergeTwoLists.merge_two_lists(l1, l2)
+        self.assertEqual(nodes_to_list(result), expected)
+
+        l1 = list_to_nodes([1])
+        l2 = list_to_nodes([2, 3, 4, 5])
+        expected = [1, 2, 3, 4, 5]
+        result = MergeTwoLists.merge_two_lists(l1, l2)
+        self.assertEqual(nodes_to_list(result), expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
