8ba7ddc352c285470a7794ccd834d751dd648742
gillerick <ogayogill95@gmail.com>
2025-05-01 20:37:54 +0300

    Re-solve valid parenthesis problem

--- 61. valid_parentheses.py
+++ 61. valid_parentheses.py
@@ -3,14 +3,23 @@ import unittest
 
 def valid_parenthesis(string_input):
     stack = []
-    brackets = {"}": "{", "]": "[", ")": "("}  # keys -> closing, values -> opening
-    for character in string_input:
-        if character in brackets.values():
-            stack.append(character)
-        elif character in brackets.keys():
-            if not stack or stack.pop() != brackets[character]:
+    brackets = {"]": "[", "}": "{", ")": "("}
+    opening_brackets = brackets.values()
+    closing_brackets = brackets.keys()
+    for char in string_input:
+        if char in opening_brackets:
+            stack.append(char)
+        elif char in closing_brackets:
+            if not stack or stack.pop() != brackets[char]:
                 return False
-    return not stack
+    return len(stack) == 0
+
+    # 1. Iterate through the input and check for an opening parenthesis
+    # 2. Push the opening parenthesis into the stack
+    # 3. If the parenthesis is closing, then check the stack for a corresponding opening
+    # 4. Remove the opening parenthesis if exists
+    # 5. If no corresponding parenthesis, then we return an early False
+    # 6. Finally, check if the stack is empty and return the boolean (stack is empty)
 
 
 class TestValidParenthesis(unittest.TestCase):
@@ -18,6 +27,7 @@ class TestValidParenthesis(unittest.TestCase):
         self.assertEqual(True, valid_parenthesis("([])(){}(())()()"))
         self.assertEqual(False, valid_parenthesis("(["))
         self.assertEqual(True, valid_parenthesis("()"))
+        self.assertEqual(False, valid_parenthesis("([]"))
 
 
 if __name__ == "__main__":
