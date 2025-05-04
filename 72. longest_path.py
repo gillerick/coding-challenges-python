d63304d6ac55888ef164146b65d91b4055df7765
gillerick <ogayogill95@gmail.com>
2025-05-04 10:36:52 +0300

    Re-solve longest path problem

--- 72. longest_path.py
+++ 72. longest_path.py
@@ -3,24 +3,24 @@ import unittest
 
 def longest_path(str_array: list[str]) -> int:
     def dfs(row, column, previous):
-        nonlocal longestPath
+        nonlocal longest_path
         if row < 0 or row >= len(matrix) or column < 0 or column >= len(matrix[0]) or matrix[row][column] <= previous:
             return 0
 
-        currentLength = 1 + max(dfs(row - 1, column, matrix[row][column]),
+        current_length = 1 + max(dfs(row - 1, column, matrix[row][column]),
                                 dfs(row + 1, column, matrix[row][column]),
                                 dfs(row, column - 1, matrix[row][column]),
                                 dfs(row, column + 1, matrix[row][column]))
 
-        longestPath = max(longestPath, currentLength)
-        return currentLength
+        longest_path = max(longest_path, current_length)
+        return current_length
 
     matrix = [[int(x) for x in row] for row in str_array]
-    longestPath = 0
+    longest_path = 0
     for row in range(len(matrix)):
         for col in range(len(matrix[0])):
             dfs(row, col, float('-inf'))
-    return longestPath - 1
+    return longest_path - 1
 
 
 class Test(unittest.TestCase):
